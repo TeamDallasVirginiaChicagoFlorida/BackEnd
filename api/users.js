5ae36fefcd6e99cefba0888b2377cb73aec96091
Lex Mullin <alemulli@gmail.com>
2022-11-22 12:01:05 -0500

    register route in api

--- api/users.js
+++ api/users.js
@@ -1,58 +1,69 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {
-    createUser,
-    getUser,
-    getUserByEmail,
-    getUserById,
-} = require("../db");
+const { createUser, getUser, getUserByEmail, getUserById } = require("../db");
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = process.env;
-const { requireUser } = require('./utils')
+const { requireUser } = require("./utils");
 
 // POST /api/users/login
 
-router.post("/login", async(req, res, next)=>{
-    const{email, password} = req.body;
+router.post("/login", async (req, res, next) => {
+  const { email, password } = req.body;
 
-    if(!email || !password){
+  if (!email || !password) {
+    next({
+      name: "MissingCredentialsError",
+      message: "Please supply both an email and a password",
+    });
+  } else {
+    try {
+      const user = await getUser({ email, password });
+      if (user) {
+        const token = jwt.sign({ id: user.id, email: user.email }, JWT_SECRET);
+        const userData = jwt.verify(token, JWT_SECRET);
+        res.send({ user, message: "You're logged in!", token });
+        return user;
+      } else {
         next({
-            name: "MissingCredentialsError",
-            message: "Please supply both an email and a password"
+          name: "IncorrectCredentialsError",
+          message: "Email or password is incorrect",
         });
-    }else{
-        try {
-            const user = await getUser({email, password});
-            if(user){
-                const token = jwt.sign(
-                    {id: user.id, email: user.email},
-                    JWT_SECRET
-                );
-                const userData=jwt.verify(token, JWT_SECRET);
-                res.send({user, message: "You're logged in!", token})
-                return user
-            }else{
-                next({
-                    name: "IncorrectCredentialsError",
-                    message: "Email or password is incorrect"
-                })
-            }
-        } catch (error) {
-            next(error)
-        }
+      }
+    } catch (error) {
+      next(error);
     }
-
-})
-
+  }
+});
 
 // POST /api/users/register
 
-router.post("/register")
+router.post("/register", async (req, res, next) => {
+    const { email, password } = req.body;
 
+    try {
+        const existingUser = await getUserByEmail(email);
+        if(existingUser){
+            next({
+                name: "UserExistsError",
+                message: `An account already exists for ${email}.`,
+            });
+        } else if (password.length < 8) {
+            next({
+                name: "PasswordTooShort",
+                message: "Password must be 8 characters or longer.",
+            })
+        } else {
+            const user = await createUser({ email,password });
+            const token = jwt.sign({ id: user.id, email: user.email }, JWT_SECRET);
+            const userData = jwt.verify(token, JWT_SECRET);
+            res.send({ user, message: "Thank you for signing up! You're logged in!", token });
+            return user;
+        }
+    } catch (error) {
+        next (error)
+    }
+});
 
 // GET /api/users/me
 
-
-
-module.exports=
-    router
+module.exports = router;
