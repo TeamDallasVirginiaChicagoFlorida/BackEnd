62ad285d10a05e73517916ba11c74fb3499ad6d8
Alex <alexwinston2123@icloud.com>
2022-11-22 13:34:38 -0600

    added .use and .delete to cars api

--- api/cars.js
+++ api/cars.js
@@ -11,12 +11,11 @@ carsRouter.get("/", async (req, res, next)=>{
 		next(error);
 }});
 
-module.exports = router;
 
 carsRouter.post("/",requireUser,
 requireActiveUser,
 async (req, res, next) => {
-  console.log("req dot body", req.body);
+	console.log("req dot body", req.body);
   const {seller,
     type,
     make,
@@ -35,19 +34,19 @@ async (req, res, next) => {
     drive_type,
     new_used = '' } = req.body;
 
-  const tagArr = tags.trim().split(/\s+/);
-  const postData = {};
-
-  try {
-	postData.seller = req.user.id
-	postData.type = type
-	postData.make = make
-	postData.model = model
-	postData.year = year
-	postData.color = color
-	postData.price= price
-	postData.transmission_type = transmission_type
-	postData.mileage =mileage
+	const tagArr = tags.trim().split(/\s+/);
+	const postData = {};
+	
+	try {
+		postData.seller = req.user.id
+		postData.type = type
+		postData.make = make
+		postData.model = model
+		postData.year = year
+		postData.color = color
+		postData.price= price
+		postData.transmission_type = transmission_type
+		postData.mileage =mileage
 	postData.interior_color = interior_color
 	postData.doors = doors
 	postData.seats = seats
@@ -57,12 +56,43 @@ async (req, res, next) => {
 	postData.drive_type = drive_type
 	postData.new_used = new_used
 	const post = await createCarPost(postData);
-
+	
 	if (post) {
-	  res.send({ post });
+		res.send({ post });
 	}
-  } catch ({ name, message }) {
+} catch ({ name, message }) {
 	next({ name, message });
-  }
+}
 }
 );
+
+carsRouter.use((req, res, next) => {
+	console.log("request is being made to /cars");
+	next();
+})
+
+carsRouter.delete('/:postId', requireUser, async (req, res, next) => {
+    try {
+      const post = await getCarById(req.params.carId);
+  
+      if (post && post.seller === req.user.id) {
+        const updatedPost = await updateCarPost(car.id, { active: false });
+  
+        res.send({ post: updatedCarPost });
+      } else {
+        // if there was a post, throw UnauthorizedUserError, otherwise throw PostNotFoundError
+        next(post ? { 
+          name: "UnauthorizedUserError",
+          message: "You cannot delete a car which is not yours"
+        } : {
+          name: "PostNotFoundError",
+          message: "That car is unavailable"
+        });
+      }
+  
+    } catch ({ name, message }) {
+      next({ name, message })
+    }
+  });
+
+module.exports = router;
