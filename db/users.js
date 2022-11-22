6be3e5123813bb4531cc14a762db79bdcf9333b9
Leonardo <cruzleonardo831@gmail.com>
2022-11-22 10:09:32 -0600

    working on users.db

--- db/users.js
+++ db/users.js
@@ -1,1 +1,52 @@
-// const express = 
+const client = require("./client")
+
+async function createUser({email, password}){
+
+    try{
+        const{
+            rows: [user]}= await client.query(
+                `INSERT INTO users(email, password, admin)
+                VALUES ($1, $2, $3)
+                ON CONFLICT (email) DO NOTHING
+                RETURNING*;
+            `,[email, password, admin]
+            );
+                return user;
+    }catch(error){
+        throw error;
+    }
+}
+
+async function getUserByEmail(userEmail){
+    try{
+        const {
+            rows:[user]}= await client.query(
+                `SELECT email, id, password FROM users,
+                WHERE  email=$1;
+                `, [userEmail]
+            ); 
+            if(!user){
+                return null;
+            }
+            return user;
+    }catch(error){
+        throw error;
+    }
+}
+
+async function getUserById(userId){
+    try{
+        const{
+            rows:[user]}=await client.query(
+                `SELECT id, username FROM users,
+                WHERE id= $1;
+                `,[userId]
+            )
+            if(!user){
+                return null
+            }return user;
+    }catch(error){
+        throw error
+    }
+}
+
