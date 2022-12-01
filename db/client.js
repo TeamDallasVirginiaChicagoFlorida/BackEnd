7a892d7c2cac158abd725cac9213ab9d4ccc2f45
irfan <ipekusic@yahoo.com>
2022-12-01 11:16:16 -0500

    created db backend

--- db/client.js
+++ db/client.js
@@ -1,5 +1,5 @@
 const { Client } = require('pg')
 
-const client = new Client(process.env.DATABASE_URL ||'postgres://localhost:5432/GraceShopper');
+const client = new Client("postgres://graceshopper_backend_user:UPTceGXvKnpF3UsUxWhRYEzHIZdaM7nw@dpg-ce4c7qhgp3jocde7o4s0-a.ohio-postgres.render.com/graceshopper_backend?ssl=true"||'postgres://localhost:5432/GraceShopper');
 
 module.exports = {client}
